c60520c80981325284a120876775ab12e74c04de
moupriyaguin <136178257+moupriyaguin@users.noreply.github.com>
2025-04-27 15:15:26 +0530

    Time: 23 ms (92.71%), Space: 46.6 MB (54.67%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefixSum = 0;
+        for(int i=0;i<nums.length;i++){
+            prefixSum += nums[i];
+            // if(prefixSum == k){
+            //     count++;
+            // }
+            // else{
+            //     // if(map.containsKey(prefixSum - k)){
+            //     //     count++;
+            //     // }
+            //     if(map.containsKey(prefixSum - k)){
+            //         count += map.get(prefixSum - k);
+            //     }
+            // }
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+            // if(!map.containsKey(prefixSum))map.put(prefixSum,i);
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
