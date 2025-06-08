2a396d9512eaf325cd971184e6b93f0c1d75252b
Moupriya Guin <136178257+moupriyaguin@users.noreply.github.com>
2025-06-08 10:18:11 +0530

    Time: 531 ms (4.95%), Space: 56.3 MB (95.84%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,20 +1,42 @@
-    class Solution {
+class Solution {
+    private void reverse(int[] nums, int from, int to) {
+        int i = from, j = to;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k=k%n;
-        int[] temp = new int[n-k];
-        for(int i=0;i<(n-k);i++){
-            temp[i] = nums[i];
-        }
+        k = k % n;
+        reverse(nums,0,n-k-1);
+        for(int i=0;i<n;i++)
+        System.out.print(nums[i]);
+        System.out.println();
+        reverse(nums,n-k,n-1);
+        for(int i=0;i<n;i++)
+        System.out.print(nums[i]);
+        System.out.println();
+        reverse(nums,0,n-1);
+        for(int i=0;i<n;i++)
+        System.out.print(nums[i]);
+        System.out.println();
+        // int[] temp = new int[n-k];
+        // for(int i=0;i<(n-k);i++){
+        //     temp[i] = nums[i];
+        // }
 
-        for(int i=n-k;i<n;i++){
-            nums[i-(n-k)] = nums[i];
-        }
+        // for(int i=n-k;i<n;i++){
+        //     nums[i-(n-k)] = nums[i];
+        // }
 
-        for(int i=k;i<n;i++){
-            nums[i] = temp[i-k];
-        }
-
+        // for(int i=k;i<n;i++){
+        //     nums[i] = temp[i-k];
+        // }
 
         // while(k>0){
         //     for(int j=n-1;j>0;j--){
