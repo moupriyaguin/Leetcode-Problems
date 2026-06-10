ae57038679554ee3e78891773406bf0e141f5fba
Moupriya Guin <136178257+moupriyaguin@users.noreply.github.com>
2026-06-11 03:54:56 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        while(i<nums.size()){
-            if(nums[i]==0){
-                break;
-            }
-            i++;
-        }
-
-        int j=i+1;
-
-        while(j<nums.size()){
-            if(nums[j]!=0){
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
                 swap(nums[i],nums[j]);
-                i++;
+                j++;
             }
-            j++;
         }
     }
 };
